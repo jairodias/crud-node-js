34a5430f4b9b5e8019e411d731077d888ab87903
Jairo <jairopereira_dias@hotmail.com>
2020-05-16 07:37:27 -0300

    criando arquivo json para configuração de email

--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -1,10 +1,9 @@
 const nodemailer = require('nodemailer');
+const { host, port, user, pass} = require('../config/mail.json');
 
 const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "b864ce7e6b6f1b",
-    pass: "f12ca74350e2cd"
-  }
+  host,
+  auth: { user, pass }
 });
+
+module.exports = transport;
