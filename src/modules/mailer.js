a3cc7b7c185d498dd21e739d0fa7cfdb2f31f128
Jairo <jairopereira_dias@hotmail.com>
2020-05-16 07:42:58 -0300

    preparação do arquivo de compilação em formato html para envio de email

--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -1,4 +1,7 @@
+const path = require('path');
 const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+
 const { host, port, user, pass} = require('../config/mail.json');
 
 const transport = nodemailer.createTransport({
@@ -6,4 +9,16 @@ const transport = nodemailer.createTransport({
   auth: { user, pass }
 });
 
+transport.use('compile', hbs({
+  viewEngine: 'handlebars', /** CONFIGURAÇÃO */
+  viewPath: path.resolve('./src/resources/mail/'), /** CAMINHO DO ARQUIVO */
+  extName: '.html', /** EXTENSÃO DO ARQUIVO */
+}));
+
 module.exports = transport;
+
+/** 
+ * PLATAFORMA: https://mailtrap.io/
+ * YARN ADD NODEMAILER 
+ * YARN ADD NODEMAILER-EXPRESS-HANDLEBARS => templates e forma de preencher variaveis em views
+*/
